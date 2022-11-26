d27249f74b623128857b2079ff82bd4541b2c409
Facundo Medrano <Facumedrano15@gmail.com>
2022-11-26 09:16:36 -0300

    Tercera preentrega

---
+++ src/stock.js
@@ -0,0 +1,8 @@
+let stockProductos = [
+    {id: 1, nombre: "Auriculares HyperX Cloud Fligth",cantidad: 1, desc: "Su sonido envolvente es del más alto nivel.", precio: 11600, img: './Public/imgs/Auriculares1.png'},
+    {id: 2, nombre: "Mouses ASUS ROG Gladius II",cantidad: 1, desc: "Desarrollado con la ayuda de gamers profesionales.", precio: 25000, img: './Public/imgs/Mouse.jpg'},
+    {id: 3, nombre: "Geforce GTX 1660 Super",cantidad: 1, desc: "Es ideal para trabajar con aplicaciones gráficas.", precio: 76000, img: './Public/imgs/1660.jpg'},
+    {id: 4, nombre: "Intel Core I3 9100f",cantidad: 1, desc: "Esencial para upgradear tu equipo dedicado al gaming y renderizado.", precio: 94799, img: './Public/imgs/i3.png'},
+    {id: 5, nombre: "AMD Ryzen 3 3200G",cantidad: 1, desc: "Procesador gama media para mejorar tu setup dedicado al gaming.", precio: 44733, img: './Public/imgs/Ryzen3.png'},
+    {id: 6, nombre: "Geforce RTX 3090",cantidad: 1, desc: "Nivel completamente nuevo de rendimiento a una resolución de 8K.", precio: 25000, img: './Public/imgs/Geforce3090.png'},
+]
